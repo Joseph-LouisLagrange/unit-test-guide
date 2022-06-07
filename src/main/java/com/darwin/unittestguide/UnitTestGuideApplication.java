ec183045eebedaccc752d2bc199316308a0e6123
Good Coder <53182805+Joseph-LouisLagrange@users.noreply.github.com>
2022-06-07 19:26:11 +0800

    init

---
+++ src/main/java/com/darwin/unittestguide/UnitTestGuideApplication.java
@@ -0,0 +1,13 @@
+package com.darwin.unittestguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitTestGuideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/darwin/unittestguide/UnitTestGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darwin.unittestguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
